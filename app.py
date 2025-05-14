5951a177ff0c26f70a34a12978514e89b45130e1
Tiju-1996 <tiju.varghese@beinex.com>
2025-05-14 18:39:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def process_risk_query(llm, user_question):
     with st.spinner("📊 Retrieving the metadata for most relevant tables..."):
         docs = retrieve_top_tables(vector_store, user_question, k=10)
         top_names = [d.metadata["table_name"] for d in docs]
-        placeholders["Top 10 Table"].markdown("## Top 10 Tables after stage 1 retrieval")
+        placeholders["Top 10 Tables"].markdown("## Top 10 Tables after stage 1 retrieval")
         placeholders["Top 10 Tables"].write(", ".join(top_names))
         example_df = pd.read_excel(examples_file)
         top3 = create_llm_table_retriever(llm, user_question, top_names, example_df)
